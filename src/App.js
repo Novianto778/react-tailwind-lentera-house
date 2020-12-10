4acbed3d0fe2b5e777af01a4e35008396d94d7d3
Novianto <novainto778@gmail.com>
2020-12-10 17:59:06 +0700

    partner section

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React from 'react'
-import Navbar from "./components/Navbar"
-import HeroSection from "./components/HeroSection"
+import React from 'react';
+import Navbar from './components/Navbar';
+import HeroSection from './components/HeroSection';
+import Partner from './components/Partner';
 
 const App = () => {
   return (
     <>
       <Navbar />
       <HeroSection />
+      <Partner />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Partner.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Partner = () => {
+  return (
+    <div className="flex flex-wrap mt-20 w-9/12 mx-auto justify-between items-center">
+      <img src="img/partner-1.png" alt="partner1" className="h-8 mt-6" />
+      <img src="img/partner-2.png" alt="partner2" className="h-6 mt-6" />
+      <img src="img/partner-3.png" alt="partner3" className="h-6 mt-6" />
+      <img src="img/partner-4.png" alt="partner4" className="h-8 mt-6" />
+      <img src="img/partner-5.png" alt="partner5" className="h-8 mt-6" />
+    </div>
+  )
+}
+
+export default Partner
